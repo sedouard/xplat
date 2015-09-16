b2e5f8b51d9cb2d85bf866d7f59b21b149fcd411
Steven Edouard <sedouard@microsoft.com>
2015-09-15 19:23:38 -0700

    wip

--- app/components/ax-select.js
+++ app/components/ax-select.js
@@ -3,15 +3,28 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     content: null,
 
+    displayContent: [],
+
     selectedValue: Ember.computed.alias('value'),
+
     targetProperty: '',
 
     didInitAttrs() {
         this._super(...arguments);
-        var content = this.get('content');
+        var content = this.get('content'),
+            optionValuePath = this.get('optionValuePath');
 
         if (!content) {
             this.set('content', []);
+        }
+
+        if (optionValuePath) {
+          this.get('content').forEach(item => {
+            this.get('displayContent').push(item.get(optionValuePath));
+          });
+        }
+        else {
+          this.set('displayContent', content);
         }
     },
 
